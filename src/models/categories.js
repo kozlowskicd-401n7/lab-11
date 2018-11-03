70d38caca1233cedae4f0197390a2148d7b52242
Chris Kozlowski <kozlowskicd@gmail.com>
2018-11-03 15:05:57 -0700

    mongoDB address changes

--- src/models/categories.js
+++ src/models/categories.js
@@ -7,36 +7,36 @@ import schema from '../lib/storage/categories-schema.js';
 const storage = new Storage(schema);
 
 class Categories {
-    static schema() {
-        return typeof schema.jsonSchema === 'function' ? schema.jsonSchema() : {};
-    }
+  static schema() {
+    return typeof schema.jsonSchema === 'function' ? schema.jsonSchema() : {};
+  }
 
-    static findOne(id) {
-      let query = { _id:id };
-      return this.find(query);
-    }
-  
-    static find(query) {
-      return storage.find(query);
-    }
-  
-    static save(data) {
-      return storage.save(data);
-    }
-  
-    static delete(id) {
-      return storage.delete(id);
-    }
-  
-    static put(id, data) {
-      return storage.save(data);
-    }
-  
-    static patch(id, data) {
-      data._id = id;
-      return storage.save(data);
-    }
-  
+  static findOne(id) {
+    let query = { _id:id };
+    return this.find(query);
   }
   
-  export default Categories
+  static find(query) {
+    return storage.find(query);
+  }
+  
+  static save(data) {
+    return storage.save(data);
+  }
+  
+  static delete(id) {
+    return storage.delete(id);
+  }
+  
+  static put(id, data) {
+    return storage.save(data);
+  }
+  
+  static patch(id, data) {
+    data._id = id;
+    return storage.save(data);
+  }
+  
+}
+  
+export default Categories;
